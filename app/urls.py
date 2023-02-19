d8ee4dfbe5f8e28602924249c01fce8efa147aff
Kamshat <komaliyeva@gmail.com>
2023-02-19 21:37:31 +0600

    added register

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app.views import index, html, register
+from app.views import index, html, register, htt
 urlpatterns = [
     path('', index),
     # path('login/', html),
     path('<filename>.html', html),
-    path("register/", register, name="register")
+    path("register/", register, name="register"),
+    path("ht/", htt)
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.tokens import default_token_generator
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
 from app.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import ObjectDoesNotExist
 
 
 def index(request):
-    if request.user.is_anonymous:
-        return redirect("/login.html")
-
+    # if request.user.is_anonymous:
+    #     return redirect("/")
     return render(request, 'index.html')
 
 
@@ -52,17 +55,41 @@ def html(request, filename):
 
 def register(request):
     if request.method == "POST":
-        if request.POST['password'] == request.POST['password2']:
-            try:
-                User.objects.get(email=request.POST['email'])
-                return HttpResponse("Такой пользователь уже есть прикинь")
-            except User.DoesNotExist:
+        try:
+            User.objects.get(email=request.POST['email'])
+            return HttpResponse("Такой пользователь уже есть прикинь")
+        except User.DoesNotExist:
+            if request.POST['password'] == request.POST['password2']:
                 user = User.objects.create_user(
                     email=request.POST['email'],
                     last_name=request.POST["last_name"],
                     first_name=request.POST['first_name'],
                     password=request.POST['password'])
+                user.save()
                 login(request, user)
                 print('New user: ', user)
                 return redirect('/')
-    return render(request, "register.html", {"user": User})
+    return render(request, 'register.html')
+
+# def reset_password(request):
+#     if request.method == 'POST':
+#         try:
+#             user = User.objects.get(email=request.POST["email"])
+#             data = {
+#                 "email": user.email,
+#                 "domain": "DOMAIN",
+#                 "site_name": "site_name",
+#                 "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+#                 "user": user,
+#                 "token": default_token_generator.make_token(user),
+#                 "protocol": "http"
+#             }
+#
+#
+#         except User.DoesNotExist:
+#             return redirect('/')
+
+
+
+def htt(request):
+    return render(request, "httt.html")
